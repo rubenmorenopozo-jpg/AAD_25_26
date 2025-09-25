f86f8c50a22075db1dbddf7282076221cba4619d
Rubén Moreno <rmorpoz2909@g.educaand.es>
2025-09-25 12:21:02 +0200

    init

---
+++ src/test/java/com/rmorpoz2909/aad/AadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmorpoz2909.aad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
